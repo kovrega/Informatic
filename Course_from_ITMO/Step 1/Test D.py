7b5e4c4a868a765757dfd241014eafadfdd34c3e
Kovrega <kovregin.andrey@gmail.com>
2024-08-14 19:11:45 +0300

    b

--- Course_from_ITMO/Step 1/Test D.py
+++ Course_from_ITMO/Step 1/Test D.py
@@ -1,16 +1,51 @@
-N = int(input())
-n = list(map(int, input().split())).sort()
-K = int(input())
-k = [tuple(map(int, input().split())) for _ in range(K)]
+
+# N = int(input())
+# n = list(map(int, input().split())).sort()
+# K = int(input())
+# k = [tuple(map(int, input().split())) for _ in range(K)]
+
+N = 5
+n = list(map(int, '10 1 10 3 4'.split())).sort()
+K = 4
+k = [
+    (1, 10),
+    (2, 9),
+    (3, 4),
+    (2, 2)
+]
+
+
+def A(l, r, n):
+    while l + 1 < r:
+        m = (l + r) // 2
+
+        if n[m] < a:
+            l = m
+        if n[m] >= a:
+            r = m
+    return r + 1
+
+def B(l, r, n):
+    while l + 1 < r:
+        m = (l + r) // 2
+
+        if n[m] <= a:
+            l = m
+        if n[m] > a:
+            r = m
+    return l + 1
 
 
 for q in k:
     a, b = q
-    l = 0
-    r = N - 1
+    l = -1
+    r = N
 
-    while l + 1 < r:
-        m = (l + r) // 2
 
-        if
+
+    print(B(l, r, n) - A(l, r, n), end=' ')
+
+
+
+
 
