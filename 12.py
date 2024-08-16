41e768e8fc29239231f2c4da9449c70434c0d7c7
Kovrega <kovregin.andrey@gmail.com>
2024-08-16 17:03:11 +0300

    b

--- 12.py
+++ 12.py
@@ -1,19 +1,24 @@
-from dataclasses import replace
+"""
+1. Заменять надо ОДИН РАЗ. т.е. N = N.replace('num', 'num', 1)
+2. Внимательно прочитать, какое финальная проверка должна быть
+"""
+
+
 
 
 def func(N : str) -> int:
     while ('52' in N) or ('2222' in N) or ('1122' in N):
         if '52' in N:
-            N = N.replace('52', '11')
+            N = N.replace('52', '11', 1)
         if '2222' in N:
-            N = N.replace('2222', '5')
+            N = N.replace('2222', '5', 1)
         if '1122' in N:
-            N = N.replace('1122', '25')
+            N = N.replace('1122', '25', 1)
 
-    return len(N)
+    return sum(list(map(int, (' '.join(N).split()))))  # ->СУММА ЦИФР в строке должна быть равна 64, а НЕ ДЛИНА строки
 
 
-for n in range(10 ** 4, 3, -1):
+for n in range(10 ** 3, 3, -1):
     if func('5' + '2' * n) == 64:
         print(n)
         break
@@ -21,13 +26,8 @@ for n in range(10 ** 4, 3, -1):
 
 
 
-
 print(int('1010000', 2))
 
-# print(bin(240)[2:])
-# print(bin(168)[2:])
-# print(bin(32)[2:])
-# print(bin(160)[2:])
 
 
 
