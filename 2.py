0c79efbe5b8e968c74dc35dcd4b8f58edcce2a98
Kovrega <kovregin.andrey@gmail.com>
2024-08-16 13:47:42 +0300

    b

---
+++ 2.py
@@ -0,0 +1,23 @@
+print('x y w z')
+for x in range(0, 2):
+    for y in range(0, 2):
+        for w in range(0, 2):
+            for z in range(0, 2):
+                if ((x and not(y)) or (y == z) or not(w)) == 0:
+                    print(x, y, w, z)
+# Вывод
+# x y w z
+# 0 0 1 1
+# 0 1 1 0
+# 1 1 1 0
+
+# Ответ: yzwx
+
+
+
+"""
+И               | or
+Или             | and
+Отрицание       | not()
+Следование      | <=
+"""
