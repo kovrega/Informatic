ad25bffe1a4b4b5bde2f70eb4a50273ead5e95c3
kovrega <kovrega.and@gmail.com>
2025-01-12 00:35:37 +0300

    10.01.25

--- СтатГрады/Январь/СтатГрад (каникулы).py
+++ СтатГрады/Январь/СтатГрад (каникулы).py
@@ -145,58 +145,128 @@
 
 
 # 9
-from collections import Counter
-
-with open("Материалы/09.csv", encoding='UTF-8') as f:
-    l = f.readlines()
-
-k = []
-for i in range(len(l)):
-    k.append(list(map(int, l[i].strip().split(';'))))
-
-column_counts = [Counter() for _ in range(6)]
-
-# Заполнение column_counts
-for row in k:
-    for col in range(6):
-        column_counts[col].update([row[col]])
-
-def check(stroka : list) -> int:
-    """
-    :return: кол-во интересных ячеек в строке
-    """
-
-    inter_cnt = []
-
-    sr_ar = sum(stroka) // 6
-    for i in range(6):
-        el = stroka[i]
-        if stroka.count(el) < 2:
-            if el > sr_ar:
-                c = 0
-                # for st in range(len(k)):
-                #     if k[st][i] == el:
-                #         c += 1
-                #
-                #     if c >= 330:
-                #         inter_cnt.append(el)
-                #         break
-                if column_counts[i][el] >= 330:
-                    inter_cnt.append(el)
-                    break
-
-    if len(inter_cnt) > 0:
-        return True
-    return False
-
-cc = 0
-for ind_stroka in range(len(k)):
-    if check(k[ind_stroka]):
-        cc += 1
-print(cc)
-
+# from collections import Counter
+#
+# with open("Материалы/09.csv", encoding='UTF-8') as f:
+#     l = f.readlines()
+#
+# k = []
+# for i in range(len(l)):
+#     k.append(list(map(int, l[i].strip().split(';'))))
+#
+# column_counts = [Counter() for _ in range(6)]
+#
+# for row in k:
+#     for col in range(6):
+#         column_counts[col].update([row[col]])
+#
+# def check(stroka : list) -> int:
+#     """
+#     :return: кол-во интересных ячеек в строке
+#     """
+#
+#     inter_cnt = []
+#
+#     sr_ar = sum(stroka) // 6
+#     for i in range(6):
+#         el = stroka[i]
+#         if stroka.count(el) < 2:
+#             if el > sr_ar:
+#                 c = 0
+#                 # for st in range(len(k)):
+#                 #     if k[st][i] == el:
+#                 #         c += 1
+#                 #
+#                 #     if c >= 330:
+#                 #         inter_cnt.append(el)
+#                 #         break
+#                 if column_counts[i][el] >= 330:
+#                     inter_cnt.append(el)
+#                     break
+#
+#     if len(inter_cnt) > 0:
+#         return True
+#     return False
+#
+# cc = 0
+# for ind_stroka in range(len(k)):
+#     if check(k[ind_stroka]):
+#         cc += 1
+# print(cc)
 
 
+# 10
+# PyDev console:
+# VSE = 2423
+# целиком = 196
+# Заголовок = 129
+# Ответ = VSE - целиком
+# Ответ
+# 2227
+# Ответ - Заголовок
+# 2098
 
 
+# 11
+
+# PyDev console:
+# код_партии = 26 * (i := 5)
+#
+# кол_во_изделий = 2
+# while 20 * (2 ** 13) >= кол_во_изделий * (40 * 8 + код_партии) + sum(j for j in range(1, кол_во_изделий + 1)):
+#     кол_во_изделий += 1
+# print(кол_во_изделий - 1)
+
+
+
+
+
+# 12
+# from random import choice
+#
+# def F(x : str) -> str:
+#     while '111' in x or '22' in x:
+#         x = x.replace('111', '2', 1)
+#         x = x.replace('222', '1', 1)
+#         x = x.replace('221', '1', 1)
+#         x = x.replace('122', '1', 1)
+#         x = x.replace('22', '2', 1)
+#     return x
+#
+# c = 0
+# for i in range(5000):
+#     s = ''.join(choice('12') for _ in range(i))
+#     # s = '1' * i + '2' * i # -> 0
+#
+#
+#     res = F(''.join(s))
+#     if res.count('2') == 9:
+#         c += 1
+#
+# print(c)
+
+
+
+
+
+
+
+
+# 13
+from ipaddress import  *
+from ipaddress import IPv4Address, ip_address, ip_network
+
+print(f"{184:b}")
+print(f"{185:b}")
+print(int('11111', 2))
+node = ip_address('157.220.185.237')
+mask = ip_address('255.255.31.0')
+
+print(f'{int(node):b}')
+print(f'{int(mask):b}')
+# node = IPv4Address('157.220.185.237')
+# mask = IPv4Network('255.255.31.0', False )
+# print(f'Адрес сети: {ip_network(f"{node}/{mask}", False) }')
+# Находим сеть
+# Находим сеть
 
