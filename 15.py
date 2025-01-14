afc9ae9de8e20078d2e258c6df949b9649f8869d
kovrega <kovrega.and@gmail.com>
2025-01-14 22:21:27 +0300

    14.01.25

--- 15.py
+++ 15.py
@@ -1,16 +1,44 @@
-a = []
-def cheching(x, y, A) -> bool:
-    return (x + 2 * y < A) or (y > x) or (x > 60)
 
-for A in range(0, 300):
-    f= True
-    for x in range(0, 300):
-        for y in range(0, 300):
-            if not(cheching(x, y ,A)):
-                f = False
+# def f(x, a1, a2):
+#     A = a1 <= x <= a2
+#     P = 12 <= x <= 62
+#     Q = 52 <= x <= 92
+#     return  (P or A) <= (Q or A)
+#
+#
+# res = []
+# ctrl_val = [y for x in (12, 62, 52, 92) for y in (x, x - 0.1, x + 0.1)]
+#
+#
+# for a1 in ctrl_val:
+#     for a2 in ctrl_val:
+#         if a2 >= a1 and all(f(x, a1, a2) for x in ctrl_val):
+#             res.append(a2 - a1)
+#
+#
+# print((min(res)))
 
-    if f:
-        a.append(A)
 
-print(min(a))
 
+
+
+
+def f(x, A):
+    return   (x & 49 != 0) <= ((x & 33 == 0) <= (x & A != 0))
+
+for A in range(1, 1000):
+    if all(f(x, A) for x in range(0, 1000) for y in range(0, 1000)):
+        print(A)
+        break
+
+
+
+
+# def f(x, A):
+#     return (not(x % A == 0)) <= ((x % 6 == 0) <= (not(x % 7 == 0)))
+#
+#
+# for A in range(1000, 1, -1):
+#     if all(f(x, A) for x in range(1, 1000)):
+#         print(A)
+#         break
--- sdo_1580/yes.py
+++ sdo_1580/yes.py
@@ -1,201 +1,169 @@
-n = 1
-l = []
-while n != 0:
-    n = int(input())
-    l.append(n)
-R = int(input())
+# with open(input()) as f:
+#     S, D  = map(int , f.readline().strip().split())
+#     cont = sorted(list(map(int, f.read().split())))
+#
+#
+# i = 0
+# s = []
+# while sum(s) + cont[i] <= S:
+#     s.append(cont[i])
+#     i += 1
+#
+# k = cont[i:]
+#
+# print(len(k), sum(k))
+#
+#
+#
 
-l = l[:-1]
-
-
-def f(l):
-    res = [str(len(l))]
-    m = 0
-    for i in range(len(l)):
-        for j in range(i + 1, len(l)):
-            if l[i] * l[j] > m and (l[i] * l[j]) % 6 == 0:
-                m = l[i] * l[j]
-
-    if m == R:
-        res.append(str(R))
-        res.append('yes')
-        return res
-    else:
-        res.append(str(m))
-        res.append('no')
-        return res
-
-
-print('\n'.join(f(l)))
-
-
-
-
-
+# with open(input()) as f:
+#     N  = int(f.readline().strip())
+#     prod = sorted(list(map(int, f.read().split())), reverse= True)
+#
+# exp = [prod[i] for i in range(N // 9)]
+#
+# prod.reverse()
+#
+# cheap = []
+# delta = [0] + [9] * (N // 9 - 1)
+# for j in range(N // 9):
+#     cheap += [ prod[j + delta[j]] ]
+#
+# print(sum(prod) - sum(exp), sum(prod) - sum(cheap))
 
 
 
 
+# with open(input()) as f:
+#     N  = int(f.readline().strip())
+#     prod = sorted(list(map(int, f.read().split())), reverse= True)
+#
+# exp = [prod[i] for i in range(N // 4)]
+# prod.reverse()
+# cheap = [prod[j] for j in range(N // 4)]
+#
+# print(sum(prod) - sum(exp)//2, sum(prod) - sum(cheap)//2)
+#
 
 
 
-# pdf_path = "C://Users//Andrey//Desktop//24.05.06, Системы управления летательными аппаратами, ИУ1 (РКТ1).pdf"
+#
+#
+# with open(input()) as f:
+#     N, S  = map(int , f.readline().strip().split())
+#     dock = sorted(list(map(int, f.read().split())), reverse=True)
 #
 #
 #
-# # Для считывания PDF
-# import PyPDF2
-# # # Для анализа структуры PDF и извлечения текста
-# from pdfminer.high_level import extract_pages, extract_text
-# from pdfminer.layout import LTTextContainer, LTChar, LTRect, LTFigure
-# # # Для извлечения текста из таблиц в PDF
-# import pdfplumber
-# # Для извлечения изображений из PDF
-# # from PIL import Image
-# # from pdf2image import convert_from_path
-# # # Для выполнения OCR, чтобы извлекать тексты из изображений
-# # import pytesseract
-# # Для удаления дополнительно созданных файлов
-# import os
+# s = []
+# i = -1
+#
+# while i + 2 < N - 1:
+#     i += 1
+#     s_ = [dock[i]]
+#
+#     while sum(s_) + dock[i + 1] <= S:
+#         s_.append(dock[i + 1])
+#         i += 1
+#
+#     for j in range(i, N - 1):
+#         if sum(s_) + dock[j + 1] <= S:
+#             s_.append(dock[j + 1])
+#             i += 1
+#             dock[j + 1], dock[i] = dock[i], dock[j + 1]
+#
+#     s.append(s_)
+#
+# if i + 1 == N - 1:
+#     s.append([dock[i + 1]])
 #
 #
-# # Извлечение таблиц из страницы
+# print(len(s), sum(s[-1]))
+
+
+
+
+
+
+
+
+
+
+from collections import Counter
+with open(input()) as f:
+    S, N  = map(int , f.readline().strip().split())
+    prod_all = sorted(list(map(int, f.read().split())))
+
+
+
+pprod = [] # [10, 10, 10, 20, 20, 30, 30]
+rprod = []
+for el in prod_all:
+    if el not in pprod:
+        c = prod_all.count(el)
+        if c > 1:
+            for k in range(c): pprod.append(el)
+
+
+# b = [] # [10, 10, 10, 20, 20]
+# i = 0
+def f(b: list , i : int):
+    while sum(b) + pprod[i] <= S:
+        c = pprod.count(pprod[i])
+        if sum(b) + 2 * pprod[i] <= S:
+            b.extend([pprod[i], pprod[i]])
+            i += 2
+            for k in range(2, c):
+                if sum(b) + pprod[i] <= S:
+                    b.append(pprod[i])
+                    i += 1
+            if i >= len(pprod) - 1:
+                break
+        else:
+            break
+    return b, i
+
+b, i = f([], 0)
+
+if  len(pprod) - i >= 2:
+    c = b.count(b[-1])
+    b_, j = f(b[:-c], i)
+    if b_ != b:
+        rprod = b_.copy()
+
+
+print(max(rprod), Counter(rprod).most_common()[0][-1])
+
+
+
+
+
+
+
+
+
+
 #
-# def extract_table(pdf_path, page_num, table_num):
-#     # Открываем файл pdf
-#     pdf = pdfplumber.open(pdf_path)
-#     # Находим исследуемую страницу
-#     table_page = pdf.pages[page_num]
-#     # Извлекаем соответствующую таблицу
-#     table = table_page.extract_tables()[table_num]
-#     return table
+# s = []
+# i = -1
 #
-# # Преобразуем таблицу в соответствующий формат
-# def table_converter(table):
-#     table_string = ''
-#     # Итеративно обходим каждую строку в таблице
-#     for row_num in range(len(table)):
-#         row = table[row_num]
-#         # Удаляем разрыв строки из текста с переносом
-#         cleaned_row = [item.replace('\n', ' ') if item is not None and '\n' in item else 'None' if item is None else item for item in row]
-#         # Преобразуем таблицу в строку
-#         table_string+=('|'+'|'.join(cleaned_row)+'|'+'\n')
-#     # Удаляем последний разрыв строки
-#     table_string = table_string[:-1]
-#     return table_string
+# while i + 2 < N - 1:
+#     i += 1
+#     s_ = [prod[i]]
 #
-# # создаём объект файла PDF
-# pdfFileObj = open(pdf_path, 'rb')
-# # создаём объект считывателя PDF
-# pdfReaded = PyPDF2.PdfReader(pdfFileObj)
+#     while sum(s_) + prod[i + 1] <= S:
+#         s_.append(prod[i + 1])
+#         i += 1
 #
-# # Создаём словарь для извлечения текста из каждого изображения
-# text_per_page = {}
-# # Извлекаем страницы из PDF
-# for pagenum, page in enumerate(extract_pages(pdf_path)):
+#     for j in range(i, N - 1):
+#         if sum(s_) + prod[j + 1] <= S:
+#             s_.append(prod[j + 1])
+#             i += 1
+#             prod[j + 1], prod[i] = prod[i], prod[j + 1]
 #
-#     # Инициализируем переменные, необходимые для извлечения текста со страницы
-#     pageObj = pdfReaded.pages[pagenum]
-#     page_text = []
-#     line_format = []
-#     text_from_images = []
-#     text_from_tables = []
-#     page_content = []
-#     # Инициализируем количество исследованных таблиц
-#     table_num = 0
-#     first_element = True
-#     table_extraction_flag = False
-#     # Открываем файл pdf
-#     pdf = pdfplumber.open(pdf_path)
-#     # Находим исследуемую страницу
-#     page_tables = pdf.pages[pagenum]
-#     # Находим количество таблиц на странице
-#     tables = page_tables.find_tables()
+#     s.append(s_)
 #
-#     # Находим все элементы
-#     page_elements = [(element.y1, element) for element in page._objs]
-#     # Сортируем все элементы по порядку нахождения на странице
-#     page_elements.sort(key=lambda a: a[0], reverse=True)
-#
-#     # Находим элементы, составляющие страницу
-#     for i, component in enumerate(page_elements):
-#         # Извлекаем положение верхнего края элемента в PDF
-#         pos = component[0]
-#         # Извлекаем элемент структуры страницы
-#         element = component[1]
-#
-#         # # Проверяем, является ли элемент текстовым
-#         # if isinstance(element, LTTextContainer):
-#         #     # Проверяем, находится ли текст в таблице
-#         #     if table_extraction_flag == False:
-#         #         # Используем функцию извлечения текста и формата для каждого текстового элемента
-#         #         (line_text, format_per_line) = text_extraction(element)
-#         #         # Добавляем текст каждой строки к тексту страницы
-#         #         page_text.append(line_text)
-#         #         # Добавляем формат каждой строки, содержащей текст
-#         #         line_format.append(format_per_line)
-#         #         page_content.append(line_text)
-#         #     else:
-#         #         # Пропускаем текст, находящийся в таблице
-#         #         pass
-#
-#         # # Проверяем элементы на наличие изображений
-#         # if isinstance(element, LTFigure):
-#         #     # Вырезаем изображение из PDF
-#         #     crop_image(element, pageObj)
-#         #     # Преобразуем обрезанный pdf в изображение
-#         #     convert_to_images('cropped_image.pdf')
-#         #     # Извлекаем текст из изображения
-#         #     image_text = image_to_text('PDF_image.png')
-#         #     text_from_images.append(image_text)
-#         #     page_content.append(image_text)
-#         #     # Добавляем условное обозначение в списки текста и формата
-#         #     page_text.append('image')
-#         #     line_format.append('image')
-#
-#         # Проверяем элементы на наличие таблиц
-#         if isinstance(element, LTRect):
-#             # Если первый прямоугольный элемент
-#             if first_element == True and (table_num + 1) <= len(tables):
-#                 # Находим ограничивающий прямоугольник таблицы
-#                 lower_side = page.bbox[3] - tables[table_num].bbox[3]
-#                 upper_side = element.y1
-#                 # Извлекаем информацию из таблицы
-#                 table = extract_table(pdf_path, pagenum, table_num)
-#                 # Преобразуем информацию таблицы в формат структурированной строки
-#                 table_string = table_converter(table)
-#                 # Добавляем строку таблицы в список
-#                 text_from_tables.append(table_string)
-#                 page_content.append(table_string)
-#                 # Устанавливаем флаг True, чтобы избежать повторения содержимого
-#                 table_extraction_flag = True
-#                 # Преобразуем в другой элемент
-#                 first_element = False
-#                 # Добавляем условное обозначение в списки текста и формата
-#                 page_text.append('table')
-#                 line_format.append('table')
-#
-#             # Проверяем, извлекли ли мы уже таблицы из этой страницы
-#             if element.y0 >= lower_side and element.y1 <= upper_side:
-#                 pass
-#             elif not isinstance(page_elements[i + 1][1], LTRect):
-#                 table_extraction_flag = False
-#                 first_element = True
-#                 table_num += 1
-#
-#     # Создаём ключ для словаря
-#     dctkey = 'Page_' + str(pagenum)
-#     # Добавляем список списков как значение ключа страницы
-#     text_per_page[dctkey] = [page_text, line_format, text_from_images, text_from_tables, page_content]
-#
-# # Закрываем объект файла pdf
-# pdfFileObj.close()
-#
-# # Удаляем созданные дополнительные файлы
-# os.remove('cropped_image.pdf')
-# os.remove('PDF_image.png')
-#
-# # Удаляем содержимое страницы
-# result = ''.join(text_per_page['Page_0'][4])
-# print(result)
-#
+# if i + 1 == N - 1:
+#     s.append([prod[i + 1]])
+
+
